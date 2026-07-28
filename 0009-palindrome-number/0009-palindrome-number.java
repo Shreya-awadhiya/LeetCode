b970316df633591ae99ba45d2d2eaf63dea549a8
Shreya-awadhiya <shreya.awadhiya16@gmail.com>
2026-07-28 23:46:05 +0530

    Time: 6 ms (22.22%), Space: 46 MB (53.65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+ if( x < 0 || (x % 110 == 0 && x != 0))
+   return false;
+
+   int temp = x;
+   int rev = 0;
+
+   while(temp > 0){
+    int digit = temp % 10 ;
+    rev =rev * 10 + digit;
+    temp /= 10;
+   }
+   return rev == x;
+    }
+}
