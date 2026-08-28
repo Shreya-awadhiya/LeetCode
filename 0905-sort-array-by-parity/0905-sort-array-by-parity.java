60fd487ce7dffbc84fd30575370148dd110e2475
Shreya Awadhiya <shreya.awadhiya16@gmail.com>
2026-08-28 23:41:23 +0530

    Time: 1 ms (46.94%), Space: 46.8 MB (50.22%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -3,15 +3,15 @@ class Solution {
         int[] arr=new int[nums.length];
         int odd=nums.length-1;
         int even=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if((nums[i]&1)==0){
-        //         arr[even]=nums[i];
-        //         even++;
-        //     }
-        //     else{
-        //         arr[odd]=nums[i];
-        //         odd--;
-        //     }
-        // }return arr;
+        for(int i=0;i<nums.length;i++){
+            if((nums[i]&1)==0){
+                arr[even]=nums[i];
+                even++;
+            }
+            else{
+                arr[odd]=nums[i];
+                odd--;
+            }
+        }return arr;
     }
 }
